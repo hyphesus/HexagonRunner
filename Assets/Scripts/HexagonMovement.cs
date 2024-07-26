3bb84dd73685542b26af7eafa0909747a8cdea15
DarthTekin <tekinteks@gmail.com>
2024-07-26 16:49:15 +0300

    HexagonMovement.cs
    HexagonMovement.cs

---
+++ Assets/Scripts/HexagonMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.ProBuilder.Shapes;
+
+public class HexagonMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+
+    float repeatZ;
+    Vector3 startPos;
+
+    //ProBuilderShape proBuilderShape;
+   
+    void Start()
+    {
+        startPos = transform.position;
+        repeatZ = GetComponent<ProBuilderShape>().size.y / 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RepeatMove();
+    }
+
+    void RepeatMove()
+    {
+        transform.Translate(Vector3.up * -speed * Time.deltaTime);
+
+        if (transform.position.z < startPos.z - repeatZ)
+        {
+            transform.position = startPos;
+        }
+    }   
+}
